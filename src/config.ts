40d1476f8a7822cbdb6c0c3eebf89b6f2b185309
RudikAleksandr <aleksandr.rudin.98@gmail.com>
2021-06-30 03:25:48 +0300

    refactor: change YouTube api key

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-const YOU_TUBE_API_KEY = 'AIzaSyDDd2p-r4Xr7-pQLf1rDAyaXCvQvgn5stw';
+const YOU_TUBE_API_KEY = 'AIzaSyB4qIdA1b7sBzE4qqbEcngD605sPydQ8iQ';
 const YOU_TUBE_API_BASE_URL = 'https://www.googleapis.com/youtube/v3';
 const YOU_TUBE_CONTENT_URL = 'http://www.youtube.com/embed';
 
